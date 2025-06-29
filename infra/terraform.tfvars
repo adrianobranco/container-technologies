43ec1983c3be001f3b42f800fd9af8157589acad
ADRIANO C BRANCO <adrianorrasec+github@gmail.com>
2025-06-29 18:18:49 +0000

    chore: create workflow and infra ECS

---
+++ infra/terraform.tfvars
@@ -0,0 +1,9 @@
+cluster_name = "app-prod"
+
+vpc_id = "vpc-0c9278c9bbfb3cf90"
+
+subnets_id = [
+  "subnet-083150dba164e6d89",
+  "subnet-0778280db592090b6",
+  "subnet-09d367f5907f2eeed"
+]
---
+++ infra/versions.tf
@@ -0,0 +1,11 @@
+cluster_name = "app-prod"
+
+terraform {
+  required_version = "1.10.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.86.0"
+    }
+  }
+}
